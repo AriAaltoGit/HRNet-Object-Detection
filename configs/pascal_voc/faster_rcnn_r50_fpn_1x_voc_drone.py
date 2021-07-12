a76e429c8ac2e73e122f21ff5a5db059d0f23aec
ari <ari.orre@aalto.fi>
2021-07-12 21:49:03 +0300

    Test config

--- configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc_drone.py
+++ configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc_drone.py
@@ -1,7 +1,7 @@
 # model settings
 model = dict(
     type='FasterRCNN',
-    pretrained='modelzoo://resnet50',
+    #pretrained='modelzoo://resnet50',
     backbone=dict(
         type='ResNet',
         depth=50,
