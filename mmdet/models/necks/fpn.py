ed4548cb085bf9d8e88e702960feb3e7889ed4fe
ari <ari.orre@aalto.fi>
2021-07-14 22:23:17 +0300

    Test config

--- mmdet/models/necks/fpn.py
+++ mmdet/models/necks/fpn.py
@@ -104,10 +104,6 @@ class FPN(nn.Module):
         # build top-down path
         used_backbone_levels = len(laterals)
         for i in range(used_backbone_levels - 1, 0, -1):
-            inter = F.interpolate(
-                laterals[i], scale_factor=2.0, mode='bicubic')
-            print("inter: ")
-            print(inter.size())
             laterals[i - 1] += F.interpolate(
                 laterals[i], scale_factor=2.0, mode='bicubic')
 
