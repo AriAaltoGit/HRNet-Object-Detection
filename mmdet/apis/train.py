b8c9ff332c732f42448dba6368204281bdc42d87
ari <ari.orre@aalto.fi>
2021-07-15 21:22:39 +0300

    Test config

--- mmdet/apis/train.py
+++ mmdet/apis/train.py
@@ -102,6 +102,7 @@ def _dist_train(model, dataset, cfg, validate=False):
         runner.resume(cfg.resume_from)
     elif cfg.load_from:
         runner.load_checkpoint(cfg.load_from)
+    print(data_loaders)
     runner.run(data_loaders, cfg.workflow, cfg.total_epochs)
 
 
