0f0cb0edbf544400e6b83decb9d048ba929dfc6d
ari <ari.orre@aalto.fi>
2021-07-16 21:19:30 +0300

    Test config

--- configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc_drone.py
+++ configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc_drone.py
@@ -17,7 +17,7 @@ model = dict(
     rpn_head=dict(
         type='RPNHead',
         in_channels=2048, #256,
-        feat_channels=2048, #256,
+        feat_channels=256,
         anchor_scales=[8],
         anchor_ratios=[0.5, 1.0, 2.0],
         anchor_strides=[4, 8, 16, 32, 64],
@@ -27,7 +27,7 @@ model = dict(
     bbox_roi_extractor=dict(
         type='SingleRoIExtractor',
         roi_layer=dict(type='RoIAlign', out_size=7, sample_num=2),
-        out_channels=2048, #256,
+        out_channels=256,
         featmap_strides=[4, 8, 16, 32]),
     bbox_head=dict(
         type='SharedFCBBoxHead',
